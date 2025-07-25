62b5f975bced9907b4082501771bdacb28727c2b
rs-vishal <vishalrs.22aid@kongu.edu>
2025-07-25 20:51:14 +0530

    week 6

---
+++ week 6/4. ReactJS-HOL/blogapp/src/Posts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+class Posts extends React.Component{
+    constructor(props) {
+        super(props);
+        //code
+    }
+
+    loadPosts() {
+        // code
+    }
+    componentDidMount() {
+        //code
+    }
+    reneder(){
+
+    }
+    componentDidCatch(error,info){
+
+    }
+}
